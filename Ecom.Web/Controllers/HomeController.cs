87fc1850649daa030d23b79f43f74aa763bdab27
jandginvestment <jandginvestment@gmail.com>
2023-09-06 06:14:44 +0400

    Adding missed functionality (posting shopping cart)

--- Ecom.Web/Controllers/HomeController.cs
+++ Ecom.Web/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using ECOM.Web.Services.IService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace ECOM.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly IProductService _productService;
-    public HomeController(IProductService productService)
+    private readonly IShoppingCartService _shoppingCartService;
+    public HomeController(IProductService productService, IShoppingCartService shoppingCartService)
     {
         _productService = productService;
+        _shoppingCartService = shoppingCartService;
     }
     public async Task<IActionResult> Index()
     {
@@ -41,5 +45,35 @@ public class HomeController : Controller
         }
         else { TempData["error"] = response?.Message; }
         return View(product);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> Details(ProductDTO product)
+    {
+        ShoppingCartDTO shoppingCart = new ShoppingCartDTO()
+        {
+
+            CartHeader = new CartHeaderDTO()
+            {
+                UserID = User.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub)?.Value,
+                CartHeaderID = 0
+            },
+            CartDetails = new List<CartDetailDTO>() { new CartDetailDTO() { Count = product.Count, ProductID = product.ProductId } }
+        };
+        var response = await _shoppingCartService.CartUpsertAsync(shoppingCart);
+        if (response != null && response.IsSuccess)
+        {
+            TempData["success"] = "Item has been added to the shopping cart";
+            return RedirectToAction(nameof(Index));
+        }
+        else { TempData["error"] = response?.Message; }
+        return View(product);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current.Id ?? HttpContext.TraceIdentifier });
     }
 }
